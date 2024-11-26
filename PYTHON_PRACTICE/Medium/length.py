81e90fb8c4ace650b4d34af54e16f1725082a616
muneer-a <muneer.a@edstem.com>
2024-11-26 09:08:40 +0530

    Create length.py

---
+++ PYTHON_PRACTICE/Medium/length.py
@@ -0,0 +1,10 @@
+'''Create a function that takes a number num and returns its length
+
+Notes :
+The use of the len() function is prohibited.'''
+
+def number_length(num):
+	sum=0
+	for i in str(num):
+		sum+=1
+	return sum
