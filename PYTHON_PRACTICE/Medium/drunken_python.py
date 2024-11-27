4acdaccb5742fe1c9b3a8e4aa1e938ab2c99415b
muneer-a <muneer.a@edstem.com>
2024-11-27 15:04:23 +0530

    Update drunken_python.py

--- PYTHON_PRACTICE/Medium/drunken_python.py
+++ PYTHON_PRACTICE/Medium/drunken_python.py
@@ -1,7 +1,8 @@
 '''
 Python got drunk and the built-in functions str() and int() are acting odd:
 
-You need to create two functions to substitute str() and int(). A function called int_to_str() that converts integers into strings and a function called str_to_int() that converts strings into integers.
+You need to create two functions to substitute str() and int(). 
+A function called int_to_str() that converts integers into strings and a function called str_to_int() that converts strings into integers.
 
 '''
 
